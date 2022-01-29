fb8a57ef51a467db72203ee0b6dc34a56aba199b
Deepak1jakhar <97865577+Deepak1jakhar@users.noreply.github.com>
2022-01-29 18:05:21 +0530

    Add files via upload

---
+++ checkingnumbers.js
@@ -0,0 +1,10 @@
+var num = prompt('enter a number: ');
+if (num>=1){
+    console.log('the number is positive');
+}
+else if (num<0){
+    console.log('the number is negative');
+}
+else{
+    console.log('zero')
+}
